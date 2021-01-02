05751c29982301328a7110cc9a406f67f267e2a0
kensuke suzuki <35k.0101@gmail.com>
2021-01-02 19:09:14 +0900

    Vueインストール

---
+++ app/javascript/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app id="app">
+    <Header />
+    <p>{{ message }}</p>
+  </v-app>
+</template>
+
+<script>
+import Header from "./components/Header";
+
+export default {
+  data: function() {
+    return {
+      message: "Hello Vue!"
+    };
+  },
+  components: {
+    Header
+  }
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
